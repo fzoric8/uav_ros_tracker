701c6010cb83accf4a6ea1090b97fc5b84574cc3
Lovro Markovic <lovro.markovic@fer.hr>
2021-01-26 11:28:42 +0100

    [mpc_tracker] Add odometry and pose subscribers

--- nodes/mpc_tracker.cpp
+++ nodes/mpc_tracker.cpp
@@ -3,6 +3,9 @@
 #include <ros/ros.h>
 #include <uav_ros_tracker/cvx_wrapper.h>
 #include <uav_ros_lib/param_util.hpp>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf2/LinearMath/Matrix3x3.h>
 
 /**
  * @brief UAV MPC Tracker implementation as found at
@@ -42,16 +45,77 @@ public:
     m_predicted_heading_trajectory =
       Eigen::MatrixXd::Zero(m_horizon_len * m_heading_state_count, 1);
 
+    m_is_active = false;
+    m_is_initialized = false;
+    m_is_tracking_in_progress = false;
+    
     // Initialize timers
     m_mpc_iteration_timer =
       m_nh.createTimer(ros::Rate(m_tracker_rate), &MPCTracker::mpc_iteration, this);
+
+    // Initialize subscribers
+    m_odom_sub = m_nh.subscribe("odometry", 1, &MPCTracker::odom_callback, this);
+    m_pose_sub = m_nh.subscribe("pose_ref", 1, &MPCTracker::pose_callback, this);
   }
 
 private:
 
-  void mpc_iteration(const ros::TimerEvent& /* unused */)
+  void mpc_iteration(const ros::TimerEvent & /* unused */)
   {
-    ROS_INFO_THROTTLE(2.0, "MPCTracker::mpc_iterations");
+    if (!m_is_initialized) {
+      ROS_INFO_THROTTLE(1.0, "MPCTracker::mpc_iterations - not initialized");
+      return;
+    }
+
+    if (!m_is_active) {
+      ROS_INFO_THROTTLE(1.0, "MPCTracker::mpc_iterations - not active");
+      return;
+    }
+
+    ROS_INFO_THROTTLE(1.0, "MPCTracker::mpc_iterations");
+  }
+
+  void odom_callback(const nav_msgs::OdometryConstPtr &msg)
+  {
+    if (!m_is_active) { set_virtual_uav_state(*msg); }
+    m_curr_odom = *msg;
+  }
+
+  void pose_callback(const geometry_msgs::PoseStampedConstPtr &msg) {
+    m_goto_pose = *msg;
+    m_is_active = true;
+  }
+
+  void set_virtual_uav_state(const nav_msgs::Odometry &msg)
+  {
+    m_mpc_state(0, 0) = msg.pose.pose.position.x;
+    m_mpc_state(1, 0) = msg.twist.twist.linear.x;
+    m_mpc_state(2, 0) = 0;
+    m_mpc_state(3, 0) = 0;
+
+    m_mpc_state(4, 0) = msg.pose.pose.position.y;
+    m_mpc_state(5, 0) = msg.twist.twist.linear.y;
+    m_mpc_state(6, 0) = 0;
+    m_mpc_state(7, 0) = 0;
+
+    m_mpc_state(8, 0) = msg.pose.pose.position.z;
+    m_mpc_state(9, 0) = msg.twist.twist.linear.z;
+    m_mpc_state(10, 0) = 0;
+    m_mpc_state(11, 0) = 0;
+
+    tf2::Matrix3x3 m(tf2::Quaternion(msg.pose.pose.orientation.x,
+      msg.pose.pose.orientation.y,
+      msg.pose.pose.orientation.z,
+      msg.pose.pose.orientation.w));
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+
+    m_mpc_heading_state(0, 0) = yaw;
+    m_mpc_heading_state(1, 0) = msg.twist.twist.linear.z;
+    m_mpc_heading_state(2, 0) = 0;
+    m_mpc_heading_state(3, 0) = 0;
+
+    m_is_initialized = true;
   }
 
   void initialize_parameters()
@@ -96,6 +160,11 @@ private:
 
   ros::NodeHandle m_nh;
   double m_tracker_rate;
+  
+  /** MPCTracker flags **/
+  bool m_is_initialized;
+  bool m_is_active;
+  bool m_is_tracking_in_progress;
 
   /* Solver parameters */
   int m_horizon_len;
@@ -135,6 +204,13 @@ private:
 
   /** Timers **/
   ros::Timer m_mpc_iteration_timer;
+
+  /** Subscribers **/
+  nav_msgs::Odometry m_curr_odom;
+  ros::Subscriber m_odom_sub;
+
+  geometry_msgs::PoseStamped m_goto_pose;
+  ros::Subscriber m_pose_sub;
 };
 
 
