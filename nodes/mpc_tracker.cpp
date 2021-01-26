ca00283400eba325c251461c22e377fba7fab2c8
Lovro Markovic <lovro.markovic@fer.hr>
2021-01-26 15:38:45 +0100

    Add 'load_trajectory' method

--- nodes/mpc_tracker.cpp
+++ nodes/mpc_tracker.cpp
@@ -1,11 +1,15 @@
 #include <memory>
 
 #include <ros/ros.h>
-#include <uav_ros_tracker/cvx_wrapper.h>
-#include <uav_ros_lib/param_util.hpp>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <tf2/LinearMath/Matrix3x3.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
+
+#include <uav_ros_tracker/cvx_wrapper.h>
+#include <uav_ros_lib/param_util.hpp>
+#include <uav_ros_lib/ros_convert.hpp>
+#include <uav_ros_lib/nonlinear_filters.hpp>
+
 
 /**
  * @brief UAV MPC Tracker implementation as found at
@@ -47,18 +51,25 @@ public:
 
     m_is_active = false;
     m_is_initialized = false;
-    m_is_tracking_in_progress = false;
-    
+
     // Initialize timers
     m_mpc_iteration_timer =
       m_nh.createTimer(ros::Rate(m_tracker_rate), &MPCTracker::mpc_iteration, this);
+    m_tracking_timer =
+      m_nh.createTimer(ros::Rate(1.0), &MPCTracker::tracking_timer, this, false, false);
 
     // Initialize subscribers
     m_odom_sub = m_nh.subscribe("odometry", 1, &MPCTracker::odom_callback, this);
     m_pose_sub = m_nh.subscribe("pose_ref", 1, &MPCTracker::pose_callback, this);
+    m_traj_sub =
+      m_nh.subscribe("trajectory_ref", 1, &MPCTracker::trajectory_callback, this);
   }
 
 private:
+  void tracking_timer(const ros::TimerEvent & /* unused */)
+  {
+    ROS_INFO_THROTTLE(1.0, "MPCTracker::tracking_timer");
+  }
 
   void mpc_iteration(const ros::TimerEvent & /* unused */)
   {
@@ -75,15 +86,103 @@ private:
     ROS_INFO_THROTTLE(1.0, "MPCTracker::mpc_iterations");
   }
 
+  void trajectory_callback(const trajectory_msgs::MultiDOFJointTrajectoryConstPtr &msg)
+  {
+    load_trajectory(*msg);
+  }
+
   void odom_callback(const nav_msgs::OdometryConstPtr &msg)
   {
     if (!m_is_active) { set_virtual_uav_state(*msg); }
     m_curr_odom = *msg;
   }
 
-  void pose_callback(const geometry_msgs::PoseStampedConstPtr &msg) {
-    m_goto_pose = *msg;
+  void pose_callback(const geometry_msgs::PoseStampedConstPtr &msg)
+  {
+    trajectory_msgs::MultiDOFJointTrajectory traj;
+    traj.points.push_back(ros_convert::to_trajectory_point(msg->pose.position.x,
+      msg->pose.position.y,
+      msg->pose.position.z,
+      msg->pose.orientation.x,
+      msg->pose.orientation.y,
+      msg->pose.orientation.z,
+      msg->pose.orientation.w));
+    load_trajectory(traj);
+  }
+
+  void load_trajectory(const trajectory_msgs::MultiDOFJointTrajectory &traj_msg)
+  {
+
+    // TODO: trajectory_msgs::MultiDOFJointTrajectory does not have delta
+    double trajectory_dt = 0.2;
+    int trajectory_size = traj_msg.points.size();
+
+    ROS_INFO_STREAM("MPCTracker::load_trajectory - recieve trajectory with "
+                    << trajectory_size << " points.");
+
+    // Allocate the desired trajectory
+    m_desired_traj_whole_x = Eigen::VectorXd::Zero(trajectory_size + m_horizon_len, 1);
+    m_desired_traj_whole_y = Eigen::VectorXd::Zero(trajectory_size + m_horizon_len, 1);
+    m_desired_traj_whole_z = Eigen::VectorXd::Zero(trajectory_size + m_horizon_len, 1);
+    m_desired_traj_whole_heading =
+      Eigen::VectorXd::Zero(trajectory_size + m_horizon_len, 1);
+
+    // Add all trajectory points
+    for (int i = 0; i < trajectory_size; i++) {
+      auto point = traj_msg.points.at(i);
+      m_desired_traj_whole_x(i) = point.transforms.front().translation.x;
+      m_desired_traj_whole_y(i) = point.transforms.front().translation.y;
+      m_desired_traj_whole_z(i) = point.transforms.front().translation.z;
+      m_desired_traj_whole_heading(i) =
+        ros_convert::calculateYaw(point.transforms.front().rotation);
+    }
+
+    // extend it so it has smooth ending
+    for (int i = 0; i < m_horizon_len; i++) {
+      m_desired_traj_whole_x(i + trajectory_size) =
+        m_desired_traj_whole_x(i + trajectory_size - 1);
+      m_desired_traj_whole_y(i + trajectory_size) =
+        m_desired_traj_whole_y(i + trajectory_size - 1);
+      m_desired_traj_whole_z(i + trajectory_size) =
+        m_desired_traj_whole_z(i + trajectory_size - 1);
+      m_desired_traj_whole_heading(i + trajectory_size) =
+        m_desired_traj_whole_heading(i + trajectory_size - 1);
+    }
+
+    // interpolate the trajectory points and fill in the desired_trajectory vector
+    for (int i = 0; i < m_horizon_len; i++) {
+
+      double first_time = m_dt1 + i * m_dt2;
+
+      int first_idx = floor(first_time / trajectory_dt);
+      int second_idx = first_idx + 1;
+
+      double interp_coeff = std::fmod(first_time / trajectory_dt, 1.0);
+
+      if (second_idx >= trajectory_size) { second_idx = trajectory_size - 1; }
+      if (first_idx >= trajectory_size) { first_idx = trajectory_size - 1; }
+
+
+      m_desired_traj_x(i, 0) = (1 - interp_coeff) * m_desired_traj_whole_x(first_idx)
+                               + interp_coeff * m_desired_traj_whole_x(second_idx);
+      m_desired_traj_y(i, 0) = (1 - interp_coeff) * m_desired_traj_whole_y(first_idx)
+                               + interp_coeff * m_desired_traj_whole_y(second_idx);
+      m_desired_traj_z(i, 0) = (1 - interp_coeff) * m_desired_traj_whole_z(first_idx)
+                               + interp_coeff * m_desired_traj_whole_z(second_idx);
+
+      m_desired_traj_heading(i, 0) =
+        nonlinear_filters::interpolate_heading(m_desired_traj_whole_heading(first_idx),
+          m_desired_traj_whole_heading(second_idx),
+          interp_coeff);
+    }
+
+    // Start the tracking timer
     m_is_active = true;
+    m_tracking_timer.setPeriod(ros::Duration(trajectory_dt));
+    m_tracking_timer.start();
+
+    ROS_INFO("MPCTracker::load_trajectory - start trajectory tracking");
+    
   }
 
   void set_virtual_uav_state(const nav_msgs::Odometry &msg)
@@ -103,13 +202,7 @@ private:
     m_mpc_state(10, 0) = 0;
     m_mpc_state(11, 0) = 0;
 
-    tf2::Matrix3x3 m(tf2::Quaternion(msg.pose.pose.orientation.x,
-      msg.pose.pose.orientation.y,
-      msg.pose.pose.orientation.z,
-      msg.pose.pose.orientation.w));
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
-
+    auto yaw = ros_convert::calculateYaw(msg.pose.pose.orientation);
     m_mpc_heading_state(0, 0) = yaw;
     m_mpc_heading_state(1, 0) = msg.twist.twist.linear.z;
     m_mpc_heading_state(2, 0) = 0;
@@ -160,11 +253,10 @@ private:
 
   ros::NodeHandle m_nh;
   double m_tracker_rate;
-  
+
   /** MPCTracker flags **/
   bool m_is_initialized;
   bool m_is_active;
-  bool m_is_tracking_in_progress;
 
   /* Solver parameters */
   int m_horizon_len;
@@ -198,19 +290,24 @@ private:
   Eigen::MatrixXd m_desired_traj_z;
   Eigen::MatrixXd m_desired_traj_heading;
 
+  Eigen::VectorXd m_desired_traj_whole_x;
+  Eigen::VectorXd m_desired_traj_whole_y;
+  Eigen::VectorXd m_desired_traj_whole_z;
+  Eigen::VectorXd m_desired_traj_whole_heading;
+
   /** Current predicted trajectory */
   Eigen::MatrixXd m_predicted_trajectory;
   Eigen::MatrixXd m_predicted_heading_trajectory;
 
   /** Timers **/
   ros::Timer m_mpc_iteration_timer;
+  ros::Timer m_tracking_timer;
 
   /** Subscribers **/
   nav_msgs::Odometry m_curr_odom;
   ros::Subscriber m_odom_sub;
-
-  geometry_msgs::PoseStamped m_goto_pose;
   ros::Subscriber m_pose_sub;
+  ros::Subscriber m_traj_sub;
 };
 
 
