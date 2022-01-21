2bd0f028808f29fe6ab350cba33fb4f59e2da753
Lovro Markovic <lovro.markovic@fer.hr>
2022-01-21 16:26:06 +0100

    [rviz_clicker] Add waypoint z-offset between frames

--- scripts/rviz_clicker.py
+++ scripts/rviz_clicker.py
@@ -2,6 +2,7 @@
 
 import rospy
 from tf.transformations import quaternion_from_euler
+import tf
 from geometry_msgs.msg import PointStamped, PoseArray, PoseStamped
 from uav_ros_msgs.msg import Waypoint, Waypoints
 from std_srvs.srv import Empty, EmptyResponse
@@ -15,25 +16,38 @@ class RvizClicker:
         self.WaitingTime = 10.0
         self.WaypointYaw = 0
         self.WaypointFrame = "eagle/carto_raw"
-       
+        self.TrackingFrame = "eagle/ekf"
+        self.ClickerToMapZ = 0
+
         # Waypoints array
         self.waypoints_msg = Waypoints()
 
         # Setup subscribers
         self.rviz_point_sub = rospy.Subscriber("/clicked_point", PointStamped, self.clicked_point_cb)
         self.rviz_pose_sub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.clicked_pose_cb)
+        self.listener = tf.TransformListener()
+
         # Setup publishers
         self.wp_array_pub = rospy.Publisher("rviz_clicker/waypoint_array", PoseArray, queue_size=1)
         self.waypoints_pub = rospy.Publisher("waypoints", Waypoints, queue_size=1)
         
         # Setup timers
         self.wp_array_timer = rospy.Timer(rospy.Duration(0.1), self.wp_array_loop)
+        self.tf_timer = rospy.Timer(rospy.Duration(0.1), self.tf_loop)
 
         # Setup services
         rospy.Service("rviz_clicker/clear_waypoints", Empty, self.clear_waypoints_srv)         
         rospy.Service("rviz_clicker/delete_last", Empty, self.delete_last_wp_srv)
         rospy.Service("rviz_clicker/publish_mission", Empty, self.publish_mission_srv)
-   
+    
+    def tf_loop(self, msg):
+        try:
+            (trans,rot) = self.listener.lookupTransform(self.WaypointFrame, self.TrackingFrame, rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            return
+
+        self.ClickerToMapZ = trans[2]
+
     def publish_mission_srv(self, msg):
         if self.waypoints_msg.waypoints:
             self.waypoints_pub.publish(self.waypoints_msg)
@@ -69,7 +83,7 @@ class RvizClicker:
         # Set position
         waypoint.pose.pose.position.x = msg.pose.position.x
         waypoint.pose.pose.position.y = msg.pose.position.y
-        waypoint.pose.pose.position.z = self.WaypointHeight
+        waypoint.pose.pose.position.z = self.WaypointHeight + self.ClickerToMapZ
         
         # Set orientation
         waypoint.pose.pose.orientation.x = msg.pose.orientation.x
@@ -91,7 +105,7 @@ class RvizClicker:
         # Set position
         waypoint.pose.pose.position.x = msg.point.x
         waypoint.pose.pose.position.y = msg.point.y
-        waypoint.pose.pose.position.z = self.WaypointHeight
+        waypoint.pose.pose.position.z = self.WaypointHeight + self.ClickerToMapZ
         
         # Set orientation
         wp_quaternion = quaternion_from_euler(0, 0, self.WaypointYaw)
